ed995870bdb517422248e84cb625ec7baf872ba5
Andrew <andrew_shliahta@ukr.net>
2021-08-14 19:17:47 +0300

    task 1 done

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
 import './sass/main.scss';
 
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
-
-const startBtn = document.querySelector('[data-action="start"]');
-const stoptBtn = document.querySelector('[data-action="stop"]');
-const body = document.querySelector('body');
-let startInterval = null;
-
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const delay = ms => {
+  // Твой код
+  return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(ms);
+  }, ms);
+});
 };
 
-const newColor = () => {
-    body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
-}
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-const startCallback = () => {
-    startInterval = setInterval(newColor, 1000);
-    startBtn.disabled = true;
-}
-const stopCallback = () => {
-    clearInterval(startInterval);
-    startBtn.disabled = false;
-}
-
-
-startBtn.addEventListener('click', startCallback);
-stoptBtn.addEventListener('click', stopCallback);
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
