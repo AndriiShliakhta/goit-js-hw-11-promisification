e77db6c85361d4de9b9891d674564ded5db95ca9
Andrew <andrew_shliahta@ukr.net>
2021-08-14 20:07:49 +0300

    update task1 and task2

--- src/index.js
+++ src/index.js
@@ -1,48 +1,13 @@
 import './sass/main.scss';
+import task1 from './js/task1';
+import task2 from './js/task2';
 
 // =============== Задание 1 ====================
-// const delay = ms => {
-//   // Твой код
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(ms);
-//     }, ms);
-//   });
-// };
-
-// const logger = time => console.log(`Resolved after ${time}ms`);
-
-// // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
-
-
+// Вызовы функции для проверки
+task1();
 
 // =============== Задание 2 ====================
-const users = [
-  { name: 'Mango', active: true },
-  { name: 'Poly', active: false },
-  { name: 'Ajax', active: true },
-  { name: 'Lux', active: false },
-];
+task2();
 
-const toggleUserState = (allUsers, userName) => {
-  return new Promise((resolve, reject) => {
-     
-    resolve(allUsers.map(user =>
-      user.name === userName ? { ...user, active: !user.active } : user, ))
-    });
-};
+// =============== Задание 3 ====================
 
-const logger = updatedUsers => console.table(updatedUsers);
-/*
- * Сейчас работает так
- */
-// toggleUserState(users, 'Mango', logger);
-// toggleUserState(users, 'Lux', logger);
-/*
- * Должно работать так
- */
-toggleUserState(users, 'Mango').then(logger);
-toggleUserState(users, 'Lux').then(logger);
