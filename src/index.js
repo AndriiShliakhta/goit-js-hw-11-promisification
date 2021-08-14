b9c2aca8d368bfac88769ec2f9ba09454f75ae89
Andrew <andrew_shliahta@ukr.net>
2021-08-14 19:40:32 +0300

    task 2 done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import './sass/main.scss';
 
-const delay = ms => {
-  // Твой код
+// =============== Задание 1 ====================
+// const delay = ms => {
+//   // Твой код
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(ms);
+//     }, ms);
+//   });
+// };
+
+// const logger = time => console.log(`Resolved after ${time}ms`);
+
+// // Вызовы функции для проверки
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
+
+
+
+// =============== Задание 2 ====================
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
   return new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(ms);
-  }, ms);
-});
+     
+    resolve(allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user, ))
+    });
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`);
-
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+const logger = updatedUsers => console.table(updatedUsers);
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
